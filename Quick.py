d4088416c221e59d76f18461199babce92c8db8b
Sangeeta Singh <80921436+sangeetasingh17@users.noreply.github.com>
2021-10-02 02:17:58 +0530

    Update Quick.py

--- Quick.py
+++ Quick.py
@@ -1,16 +1,16 @@
 def partition(arr, start, end):
-    i=start-1
-    pivot= arr[end]
+    i=start-1        # index of smaller element
+    pivot= arr[end]  # pivot
     for j in range(start, end):
-        if arr[j]<=pivot:
-            i = i+1 
+        if arr[j]<=pivot:    # If current element is smaller than or equal to pivot
+            i = i+1          # increment index of smaller element
             arr[i],arr[j] = arr[j],arr[i] 
     arr[i+1],arr[end] = arr[end],arr[i+1]
     return (i+1) 
 
 def quicksort(arr, start, end):
-    if start<end:
-        pivot= partition(arr, start, end)
+    if start<end:    
+        pivot= partition(arr, start, end)    # pivot is partitioning index, arr[pivot] is now at right place
         quicksort(arr, start, pivot-1)
         quicksort(arr, pivot+1, end)
 f=open("text.txt","r")
